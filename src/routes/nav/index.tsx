47db6cdca0ba606c5f3fa981da9a3d09b9f86ece
jaggi47 <iamjagmohansingh17@gmail.com>
2022-08-24 10:21:37 +0530

    ON-02: toast container

--- src/routes/nav/index.tsx
+++ src/routes/nav/index.tsx
@@ -3,19 +3,17 @@
  * @author Jagmohan Singh
  */
 import "react-toastify/dist/ReactToastify.css";
-import { connect } from "react-redux";
 import styles from "./index.module.scss";
 import { IoIosNotificationsOutline } from "react-icons/io"
 import { FiChevronDown } from "react-icons/fi"
+import { useState } from "react";
 import NotificationPopup from '../../components/NotificationPopup/index'
-import { Modal } from '../../components/Portal'
-import { useRef, useState } from "react";
 
 type Props = {
   showUserData?: boolean
 }
 
-const Nav = ({ showUserData }: Props) => {
+const Nav = ({ showUserData = true }: Props) => {
   const [notificationvisibility, setNotificationvisibility] = useState(false);
 
 
@@ -24,46 +22,27 @@ const Nav = ({ showUserData }: Props) => {
       <div className={styles["nav-head"]}>Onsite Travel</div>
 
 
-      { showUserData && 
-      <>
-      <div className={styles["notification"]}>
-      <IoIosNotificationsOutline className={styles["notification-icon"]}
-        onClick={() => setNotificationvisibility(!notificationvisibility)}
-      />
-      {notificationvisibility && <NotificationPopup  onClickOutside={() => {setNotificationvisibility(false)}} />}
-    </div>
-      <div className={styles["user-data"]}>
-        <div className={styles["image-container"]}>
-          <img src="https://sneakers-app.s3.amazonaws.com/staging/images/small/staging-image-1658749914901-223" className={styles["user-image"]} alt="profile" />
-        </div>
-        <div className={styles["user-info"]}>
-          <div className={styles["user-welcome"]}>Welcome,</div>
-          <div className={styles["user-name"]}>Steven Walter</div>
-        </div>
-        <FiChevronDown className={styles["down-icon"]} />
-      </div>
-      </>}
-
+      {showUserData &&
+        <>
+          <div className={styles["notification"]}>
+            <IoIosNotificationsOutline className={styles["notification-icon"]}
+              onClick={() => setNotificationvisibility(!notificationvisibility)}
+            />
+            {notificationvisibility && <NotificationPopup onClickOutside={() => { setNotificationvisibility(false) }} />}
+          </div>
+          <div className={styles["user-data"]}>
+            <div className={styles["image-container"]}>
+              <img src="https://sneakers-app.s3.amazonaws.com/staging/images/small/staging-image-1658749914901-223" className={styles["user-image"]} alt="profile" />
+            </div>
+            <div className={styles["user-info"]}>
+              <div className={styles["user-welcome"]}>Welcome,</div>
+              <div className={styles["user-name"]}>Steven Walter</div>
+            </div>
+            <FiChevronDown className={styles["down-icon"]} />
+          </div>
+        </>}
     </section>
   );
 }
 
-// handles the outgoing dispatches
-const mapDispatchToProps = () => {
-  return {};
-};
-
-// handles incoming state changes
-const mapStateToProps = () => {
-  return {};
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Nav);
-function useEffect(arg0: () => () => void, arg1: never[]) {
-  throw new Error("Function not implemented.");
-}
-
-function handleClickOutside(arg0: string, handleClickOutside: any, arg2: boolean) {
-  throw new Error("Function not implemented.");
-}
-
+export default Nav;
