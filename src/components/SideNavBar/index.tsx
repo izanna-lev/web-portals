ce1228cfc90ce099ea4a0575d442d5abc385946a
jaggi47 <iamjagmohansingh17@gmail.com>
2022-09-27 12:21:59 +0530

    chat: access

--- src/components/SideNavBar/index.tsx
+++ src/components/SideNavBar/index.tsx
@@ -2,31 +2,37 @@ import NavigationOption from "../sub-components/NavigationOption";
 import { ICON } from "../../constants";
 import styles from "./index.module.scss";
 import { NAVIGATE } from "../../constants";
+import { useAppSelector } from "../../store/hooks";
 
-const SideNavBar = () => (
-  <nav className={`${styles["navigation-sidebar"]} `}>
-    {NavigationOption("Dashboard", ICON.DASHBOARD_INACTIVE, NAVIGATE.DASHBOARD)}
-    {NavigationOption(
-      "Assigned Itineraries",
-      ICON.ITINERARIES_INACTIVE,
-      NAVIGATE.ITINERARY,
-      "itinerary/list"
-    )}
-    {NavigationOption("Chat", ICON.CHAT_INACTIVE, NAVIGATE.CHAT)}
-    {NavigationOption(
-      "Cancel Request",
-      ICON.CANCELLED_ITINERARIES_INACTIVE,
-      NAVIGATE.CANCELLED_ITINERARIES,
-      "itinerary/cancelled"
-    )}
-    {NavigationOption(
-      "Send Notifications",
-      ICON.NOTIFICATIONS_INACTIVE,
-      NAVIGATE.NOTIFICATION,
-      "notifications"
-    )}
-    {NavigationOption("Profile", ICON.PROFILE_INACTIVE, NAVIGATE.PROFILE)}
-  </nav>
-);
+const SideNavBar = () => {
+     const profileData = useAppSelector((state) => state.profile);
+
+  return  (
+  
+    <nav className={`${styles["navigation-sidebar"]} `}>
+      {NavigationOption("Dashboard", ICON.DASHBOARD_INACTIVE, NAVIGATE.DASHBOARD)}
+      {NavigationOption(
+        "Assigned Itineraries",
+        ICON.ITINERARIES_INACTIVE,
+        NAVIGATE.ITINERARY,
+        "itinerary/list"
+      )}
+      {NavigationOption("Chat", ICON.CHAT_INACTIVE, NAVIGATE.CHAT)}
+      {NavigationOption(
+        "Cancel Request",
+        ICON.CANCELLED_ITINERARIES_INACTIVE,
+        NAVIGATE.CANCELLED_ITINERARIES,
+        "itinerary/cancelled"
+      )}
+      { profileData.access?.sendNotifications && NavigationOption(
+        "Send Notifications",
+        ICON.NOTIFICATIONS_INACTIVE,
+        NAVIGATE.NOTIFICATION,
+        "notifications"
+      )}
+      {NavigationOption("Profile", ICON.PROFILE_INACTIVE, NAVIGATE.PROFILE)}
+    </nav>
+  );
+}
 
 export default SideNavBar;
