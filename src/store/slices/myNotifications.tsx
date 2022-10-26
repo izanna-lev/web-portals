1a8d8b15a03fc252aad885ecb3d4266eec02f028
Shivender-Kun <shivenderkumar761@gmail.com>
2022-10-26 11:11:33 +0530

    notification Popup fetch

---
+++ src/store/slices/myNotifications.tsx
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { API } from "../../constants";
+
+const initialState = { list: [] };
+
+const MyNotifications = createSlice({
+  name: "MyNotifications",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(API.NOTIFICATION_LIST, (state, action: any) => {
+      Object.assign(state, { list: [action.payload] });
+    });
+  },
+});
+
+export default MyNotifications.reducer;
