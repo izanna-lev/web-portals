3b2cc96db683017b2e230a6bf633aef894042bb5
Shivender-Kun <shivenderkumar761@gmail.com>
2023-01-05 17:17:25 +0530

    firebase double noti fixed

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -19,15 +19,14 @@ const messaging = firebase.messaging();
 console.log(messaging);
 
 messaging.onBackgroundMessage((payload) => {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload
-  );
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: payload.notification.image,
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  // console.log(
+  //   "[firebase-messaging-sw.js] Received background message ",
+  //   payload
+  // );
+  // const notificationTitle = payload.notification.title;
+  // const notificationOptions = {
+  //   body: payload.notification.body,
+  //   icon: payload.notification.image,
+  // };
+  // self.registration.showNotification(notificationTitle, notificationOptions);
 });
