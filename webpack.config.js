95e1e0f8ad47954be5e5f3a72de22cb63c517bbb
Shivender-Kun <shivenderkumar761@gmail.com>
2023-03-30 09:59:00 +0530

    config updated

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
  * @author Shivender
  **/
 
-// const CompressionPlugin = require("compression-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
@@ -142,10 +142,10 @@ module.exports = (env) => {
         ],
       }),
       new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
-      // new CompressionPlugin({
-      //   algorithm: "gzip",
-      //   test: /.js$|.css$|.map$/,
-      // }),
+      new CompressionPlugin({
+        algorithm: "gzip",
+        test: /.js$|.css$|.map$/,
+      }),
       new HtmlWebpackPlugin({
         template: "./public/index.html",
         favicon: "./public/favicon.png",
